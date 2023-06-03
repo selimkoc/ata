9668f8cf605e6479a7adba139e0d22aa6f8ab88d
Selim Koc <selimkoc@gmail.com>
2023-06-03 21:47:51 +0300

    remove index.html files and parantheses from inc

--- inc.php
+++ inc.php
@@ -3,25 +3,25 @@
 namespace ATA;
 
 // CONFIGURATION
-require_once(ATA_PLUGIN_DIR . '/config/config.php');
-require_once(ATA_PLUGIN_DIR . '/config/text.php');
+require_once ATA_PLUGIN_DIR . '/config/config.php';
+require_once ATA_PLUGIN_DIR . '/config/text.php';
 
 // HELPERS
-require_once(ATA_PLUGIN_DIR . '/app/helpers/ata.php');
+require_once ATA_PLUGIN_DIR . '/app/helpers/ata.php';
 
 // BASE CLASSES
-require_once(ATA_PLUGIN_DIR . '/app/base/core.php');
-require_once(ATA_PLUGIN_DIR . '/app/base/controller.php');
-require_once(ATA_PLUGIN_DIR . '/app/base/model.php');
-require_once(ATA_PLUGIN_DIR . '/app/base/api.php');
+require_once ATA_PLUGIN_DIR . '/app/base/core.php';
+require_once ATA_PLUGIN_DIR . '/app/base/controller.php';
+require_once ATA_PLUGIN_DIR . '/app/base/model.php';
+require_once ATA_PLUGIN_DIR . '/app/base/api.php';
 
 // ROUTERS
-require_once(ATA_PLUGIN_DIR . '/app/router/router.php');
-require_once(ATA_PLUGIN_DIR . '/app/router/ajax.php');
-require_once(ATA_PLUGIN_DIR . '/app/router/api.php');
-require_once(ATA_PLUGIN_DIR . '/app/router/url.php');
-require_once(ATA_PLUGIN_DIR . '/app/router/post.php');
+require_once ATA_PLUGIN_DIR . '/app/router/router.php';
+require_once ATA_PLUGIN_DIR . '/app/router/ajax.php';
+require_once ATA_PLUGIN_DIR . '/app/router/api.php';
+require_once ATA_PLUGIN_DIR . '/app/router/url.php';
+require_once ATA_PLUGIN_DIR . '/app/router/post.php';
 
 // APP
 // ATA Class
-require_once(ATA_PLUGIN_DIR . '/app/main/ata.php');
+require_once ATA_PLUGIN_DIR . '/app/main/ata.php';
